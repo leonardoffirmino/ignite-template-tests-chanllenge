4e5904334b63654e49f2b59d0569a4b1cf2bc9d2
leonardoffirmino <firmino750@gmail.com>
2023-10-09 20:25:22 -0300

    Ajusted test create a new user

--- src/modules/users/useCases/createUser/CreateUserUseCase.spec.ts
+++ src/modules/users/useCases/createUser/CreateUserUseCase.spec.ts
@@ -1,4 +1,5 @@
 import { InMemoryUsersRepository } from "../../repositories/in-memory/InMemoryUsersRepository";
+import { CreateUserError } from "./CreateUserError";
 import { CreateUserUseCase } from "./CreateUserUseCase";
 import { ICreateUserDTO } from "./ICreateUserDTO";
 
@@ -24,6 +25,33 @@ describe("Create a new user", () => {
     };
 
     await createUserUseCase.execute(user);
+
+    const userCreated = await usersRepositoryInMemory.findByEmail(user.email);
+
+
+    expect(userCreated).toHaveProperty("id");
+    expect(userCreated).toHaveProperty("name", user.name);
+
   });
+
+
+  it("should not be able to create more than one user with the same email", async () => {
+    const user: ICreateUserDTO = {
+      name: "Test user",
+      email: "test@rocket.com.br",
+      password: "1234"
+    };
+
+    await createUserUseCase.execute(user);
+
+    await expect(
+      createUserUseCase.execute({
+        name: "Test User2",
+        email: "test@mail.com",
+        password: "1234",
+      })
+    ).rejects.toEqual(new CreateUserError());
+  });
+
 
 });
