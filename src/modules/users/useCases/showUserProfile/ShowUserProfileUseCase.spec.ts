c5d1307ddb6e8876aa932b46a08d22e40f5c9ccb
leonardoffirmino <firmino750@gmail.com>
2023-10-17 22:47:01 -0300

    Modify show profile

--- src/modules/users/useCases/showUserProfile/ShowUserProfileUseCase.spec.ts
+++ src/modules/users/useCases/showUserProfile/ShowUserProfileUseCase.spec.ts
@@ -1,7 +1,15 @@
+import { InMemoryUsersRepository } from "../../repositories/in-memory/InMemoryUsersRepository";
+import { CreateUserUseCase } from "../createUser/CreateUserUseCase";
+import { ShowUserProfileUseCase } from "./ShowUserProfileUseCase";
 
 
+let userRepositoryInMemory: InMemoryUsersRepository;
+let createUserUseCase: CreateUserUseCase;
+let showUserProfileUsecase: ShowUserProfileUseCase;
 
 
 describe("Show user profile", () => {
+  beforeEach(() => {
 
+  })
 });
